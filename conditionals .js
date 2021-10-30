28359664b20b262ce0876e2a4b946c6784438663
Arijit Nnadi <arijitnandi240@gmail.com>
2021-10-30 23:45:19 +0530

    number practice commit done

---
+++ conditionals .js
@@ -0,0 +1,21 @@
+// How many even numbers are present from 0 to n
+// node conditionals.js
+// var n=5;
+// var i=0;
+
+// var rem=n%2
+
+// if(rem==0){
+//     console.log("even")
+// }else{
+//     console.log("odd")
+// }
+// var n=10;
+
+ 
+for(i=0;i<=n;i++){
+    if(i%2==0){
+        console.log(i):
+    }
+    
+} 
