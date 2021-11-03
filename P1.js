038f809f7d2d1fa18246347b870595f0a51ed71c
Arijit Nnadi <arijitnandi240@gmail.com>
2021-11-03 23:31:11 +0530

    Practice commits

---
+++ P1.js
@@ -0,0 +1,42 @@
+// Check a number is greater than other or not and print.
+// var num1=10;
+// var num2=90;
+// if (num1>num2){
+//     console.log(num1,"is greated than",num2);
+// }else{
+//     console.log(num2,"is greated than",num1);
+// }
+// --------------------------------------------------
+// if (2){
+//     console.log("print if block");
+// }else{
+//     console.log("print else block");
+// }
+// ------------------------------------------------------
+// var num1=10;
+// var num2=3;
+// var rem=num1%num2;
+// console.log(rem);
+// if(rem%2===0){
+//     console.log(rem,"is devided by 2");
+// }else{
+//     console.log(rem,"is not devided by 2");
+// }
+// ---------------------------------------------------------
+// write a program to check a program is prime or not and print.
+
+// const num1=100.3;
+
+// if(num1<1){
+//     console.log(num1,"is a negetive integer");
+// }else if(num1===1){
+//     console.log(num1,"is neither prime nor composite");
+// }else if(num1===2 || num1===3 || num1===5 || num1===7){
+//     console.log(num1,"is a prime number")
+// }else if(num1%2===0 || num1%3===0 || num1%5===0 || num1%7===0){
+//     console.log(num1,"is not a prime number");
+// }else{
+//     console.log(num1,"is a prime number")
+// }
+// ----------------------------------------------------
+
